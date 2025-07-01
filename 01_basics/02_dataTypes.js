d7da1e136baebbbeba259b31e37109161529db4d
Raghav <rjhunjhunwala095@gmail.com>
2025-07-01 14:28:10 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert(3 + 3)  // we are using nodeJS, not browser
+
+console.log("Raghav");
+
+let name = "Raghav"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbmol => unique
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
