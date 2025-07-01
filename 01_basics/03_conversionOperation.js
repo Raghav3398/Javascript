11f3640c61ed459bb46331097b231ed13115d70a
Raghav <rjhunjhunwala095@gmail.com>
2025-07-01 14:55:39 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Raghav"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // // console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "raghav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "raghav" => true
+
+let somenumber = 33
+
+let stringNumeber = String(somenumber)
+console.log(stringNumeber);
+console.log(typeof stringNumeber);
